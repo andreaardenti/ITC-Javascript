bf9fb05884e50f3790f78f980d8ded2b76642efd
Andrea Ardenti <andreaardenti@users.noreply.github.com>
2018-10-25 13:07:49 +0200

    Update npmRomanNumerals.js

--- npmRomanNumerals.js
+++ npmRomanNumerals.js
@@ -1,1 +1,7 @@
+// dal prompt > inpm install --save roman-numeral
 console.log(require('roman-numeral').convert(1972));
+
+
+// dal prompt > install npm decimal
+var Decimal = require('decimal');
+console.log(Decimal('10.01').add('23.00942').toNumber());
