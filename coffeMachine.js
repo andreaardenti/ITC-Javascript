6cde55da2a9e9a6f0b529c48c0140d352b1e1f8c
Andrea Ardenti <andreaardenti@users.noreply.github.com>
2018-10-26 19:42:27 +0200

    Update coffeMachine.js

--- coffeMachine.js
+++ coffeMachine.js
@@ -1,24 +1,36 @@
-let toConvert = 0;
-
 function money(value) {
-    let size = {
-        "500":0,
-        "200":0,
-        "100":0,
-        "50":0,
-        "20":0,
-        "10":0,
-        "5":0,
-        "2":0,
-        "1":0
-    }, counter = '', i;
-    for (i in size) {
-        while (value >= size[i]) {
-            counter += size[i];
-            value -= size[i];
+    let size = { "500": 0, "200": 0, "100": 0, "50": 0, "20": 0, "10": 0, "5": 0, "2": 0, "1": 0 };
+    while (value != 0) {
+        if (value % 500 === 0) {
+            size["500"]++;
+            value -= 500;
+        } else if (value % 200 === 0) {
+            size["200"]++;
+            value -= 200;
+        } else if (value % 100 === 0) {
+            size["100"]++;
+            value -= 100;
+        } else if (value % 500 === 0) {
+            size["50"]++;
+            value -= 50;
+        } else if (value % 20 === 0) {
+            size["20"]++;
+            value -= 20;
+        } else if (value % 10 === 0) {
+            size["10"]++;
+            value -= 10;
+        } else if (value % 5 === 0) {
+            size["5"]++;
+            value -= 5;
+        } else if (value % 2 === 0) {
+            size["2"]++;
+            value -= 2;
+        } else if (value % 1 === 0) {
+            size["1"]++;
+            value -= 1;
         }
     }
-    return JSON.stringify({counter});
+    return size;
 }
 
-console.log(money(410));
+console.log(money(12321));
