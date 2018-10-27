6ac0289672cfcab9d58c7f9ef29333f266da75e9
Andrea Ardenti <andreaardenti@users.noreply.github.com>
2018-10-27 10:41:31 +0200

    Update coffeMachine.js

--- coffeMachine.js
+++ coffeMachine.js
@@ -1,48 +1,43 @@
 function money(value) {
-    
-    //inizializzo l'oggetto JSON
     let size = { "500": 0, "200": 0, "100": 0, "50": 0, "20": 0, "10": 0, "5": 0, "2": 0, "1": 0 };
-    
-    //imposto che il numero inserito sia positivo, altrimenti visualizzo messaggio di errore
     if (value > 0) {
-        //controllo che sia diverso da zero e faccio partire il controllo
         while (value != 0) {
-            if (value % 500 === 0) {
+            if (value >= 500) {
                 size["500"]++;
                 value -= 500;
-            } else if (value % 200 === 0) {
+            } else if (value >= 200) {
                 size["200"]++;
                 value -= 200;
-            } else if (value % 100 === 0) {
+            } else if (value >= 100) {
                 size["100"]++;
                 value -= 100;
-            } else if (value % 500 === 0) {
+            } else if (value >= 50) {
                 size["50"]++;
                 value -= 50;
-            } else if (value % 20 === 0) {
+            } else if (value >= 20) {
                 size["20"]++;
                 value -= 20;
-            } else if (value % 10 === 0) {
+            } else if (value >= 10) {
                 size["10"]++;
                 value -= 10;
-            } else if (value % 5 === 0) {
+            } else if (value >= 5) {
                 size["5"]++;
                 value -= 5;
-            } else if (value % 2 === 0) {
+            } else if (value >= 2) {
                 size["2"]++;
                 value -= 2;
-            } else if (value % 1 === 0) {
+            } else if (value >= 1) {
                 size["1"]++;
                 value -= 1;
             }
         }
     } else {
-        console.log('Spiacente ma il numero inserito è negativo!');
+        console.log('\nSpiacente ma il numero inserito è negativo!');
     }
     return size;
 }
 
-let a = 2541;
-let b = -2541
+let a = 67;
+let b = -67
 console.log("---- Stampa del JSON positivo ----\n", money(a));
 console.log("---- Stampa del JSON negativo ----\n", money(b));
